167288078216991172502559b5faad1eb4e7a0ea
zvkemp <zvkemp@gmail.com>
2014-03-12 15:10:19 -0700

    comparison operators

--- spec/sorabji/parser_spec.rb
+++ spec/sorabji/parser_spec.rb
@@ -236,6 +236,22 @@ describe Sorabji::Parser do
   end
 
   describe "comparisons" do
+    [
+      ["1 > 2", false],
+      ["1<2", true],
+      ["1 <= 2", true],
+      ["1 >= 2", false],
+      ["1 == 2", false],
+      ["1 == 1", true],
+      ["1 >= 1", true],
+      ["1 <= 1", true],
+      ["1 < {276}", true],
+      ["1 > {276}", false]
+    ].each do |example, expectation|
+      specify "comparison #{example}" do
+        parse(example).to_ast[0].to_proc.call({ 276 => 2 }).must_equal expectation
+      end
+    end
   end
 
   describe "named functions" do

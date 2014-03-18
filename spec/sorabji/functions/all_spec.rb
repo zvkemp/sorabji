9facfa57478c35f3c5299bf9a649f9722a0c27ca
zvkemp <zvkemp@gmail.com>
2014-03-18 16:01:23 -0700

    all? function uses a question mark

--- spec/sorabji/functions/all_spec.rb
+++ spec/sorabji/functions/all_spec.rb
@@ -3,10 +3,10 @@ require_relative '../../spec_helper'
 describe Sorabji::FunctionAll do
 
   [
-    ["all[101 102 103]", { 101 => "", 102 => "yes", 103 => "maybe" }, true],
-    ["all{101 102 103}", { 101 => "", 102 => "yes", 103 => "maybe" }, false],
-    ["all{101 102 103}", { 101 => 1, 102 => 2, 103 => 3 },            true],
-    ["all{101 102 103}", { 101 => :hello, 102 => " ", 103 => "3" },   false]
+    ["all?[101 102 103]", { 101 => "", 102 => "yes", 103 => "maybe" }, true],
+    ["all?{101 102 103}", { 101 => "", 102 => "yes", 103 => "maybe" }, false],
+    ["all?{101 102 103}", { 101 => 1, 102 => 2, 103 => 3 },            true],
+    ["all?{101 102 103}", { 101 => :hello, 102 => " ", 103 => "3" },   false]
   ].each do |example, object, expectation, desc|
     describe desc do
       let(:function){ parse(example).to_ast.to_proc }
@@ -16,15 +16,15 @@ describe Sorabji::FunctionAll do
 
   describe "mixing functions" do
     specify 'within an if function' do
-      function = parse(%{if[all{101 102} "yes" "no"]}).to_ast.to_proc
+      function = parse(%{if[all?{101 102} "yes" "no"]}).to_ast.to_proc
       function.call({ 101 => 2 }).must_equal 'no'
       function.call({ 101 => 2, 102 => 3 }).must_equal 'yes'
     end
 
     specify 'containing an if function' do
-      function = parse(%{all[if[1 > 0 true false] true]}).to_ast.to_proc
+      function = parse(%{all?[if[1 > 0 true false] true]}).to_ast.to_proc
       function.call({}).must_equal true
-      function = parse(%{all[if[1 < 0 true false] true]}).to_ast.to_proc
+      function = parse(%{all?[if[1 < 0 true false] true]}).to_ast.to_proc
       function.call({}).must_equal false
     end
   end

24e95f9957a590510e7fc2363e183babd2012c27
zvkemp <zvkemp@gmail.com>
2014-04-18 10:47:16 -0700

    additional specs

--- spec/sorabji/functions/concat_spec.rb
+++ spec/sorabji/functions/concat_spec.rb
@@ -3,11 +3,15 @@ require_relative '../../spec_helper'
 describe Sorabji::FunctionConcat do
   [
     [%{concat[101 102 ";"]}, {}, "101;102", %{simple values}],
-    [%{concat[present{101 102 103} "; "]}, { 101 => "yes", 103 => "present" }, "yes; present", %{missing lookup}]
+    [%{concat[present{101 102 103} "; "]}, { 101 => "yes", 103 => "present" }, "yes; present", %{missing lookup}],
+    [%{concat[{101} {102} "; "]}, {}, "", %{2 missing lookups}],
+    [%{concat[{101 102} "; "]}, {}, "", %{2 missing lookups 2}],
+    [%{concat[present{101 102} "; "]}, {}, "", %{2 missing lookups 3}]
   ].each do |example, object, expectation, desc|
     describe desc do
       let(:function){ parse(example).to_ast.to_proc }
       specify { function.call(object).must_equal expectation }
     end
   end
+
 end
